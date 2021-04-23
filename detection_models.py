e011d17821651d1eb8bd7365aa841eecc2ea9fad
nmushkin <noah@whatsbusy.com>
2021-04-22 22:33:50 -0400

    Backbone fixes

--- detection_models.py
+++ detection_models.py
@@ -10,29 +10,33 @@ import torch
 
 def get_fasterrcnn_model(num_classes, feature_extract_only=False):
 
-    anchor_gen = AnchorGenerator(sizes=((16, 32, 64, 128, 256), ))
     model = fasterrcnn_resnet50_fpn(pretrained=True)
     if feature_extract_only:
         set_grad_required(model, not feature_extract_only)
+
+    anchor_gen = AnchorGenerator(sizes=((16, 32, 64, 128, 256), ))
+    rpn_head = RPNHead(256, anchor_gen.num_anchors_per_location()[0])
+
+    rpn_pre_nms_top_n = dict(training=2000, testing=1000)
+    rpn_post_nms_top_n = dict(training=2000, testing=1000)
+    model.rpn = RegionProposalNetwork(
+        anchor_generator=anchor_gen,
+        head=rpn_head,
+        fg_iou_thresh=0.7,
+        bg_iou_thresh=0.3,
+        batch_size_per_image=256,
+        positive_fraction=0.5,
+        pre_nms_top_n=rpn_pre_nms_top_n,
+        post_nms_top_n=rpn_post_nms_top_n,
+        nms_thresh=0.7
+    )
+
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(
         in_features,
         num_classes + 1  # +1 for background class
-    )
-
-    rpn_head = RPNHead(256, anchor_gen.num_anchors_per_location()[0])
-    # RPN parameters
-
-    rpn_pre_nms_top_n = dict(training=2000, testing=1000)
-    rpn_post_nms_top_n = dict(training=2000, testing=1000)
-    model.rpn = RegionProposalNetwork(
-        anchor_gen, rpn_head,
-        0.7, 0.3,
-        256, 0.5,
-        rpn_pre_nms_top_n, rpn_post_nms_top_n, 0.7,
-        score_thresh=0.0
     )
 
     return model
